69f5a82808598308baa7298b79b8818219268d74
Fizza-A786 <atesh7865@gmail.com>
2026-09-16 01:48:57 +0500

    removed mongo db

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,27 +1,11 @@
 import express from "express";
-import cors from "cors";
-
 import userRoutes from "./routes/userRoutes";
 
 const app = express();
 
-// ==========================================
-// MIDDLEWARE
-// ==========================================
-
-app.use(cors());
-
 app.use(express.json());
 
-// ==========================================
-// USER ROUTES
-// ==========================================
-
 app.use("/api/users", userRoutes);
-
-// ==========================================
-// SERVER
-// ==========================================
 
 const PORT = process.env.PORT || 5000;
 
